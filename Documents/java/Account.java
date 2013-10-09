f49ef36ba8372c5ecf2a12f357cb9feae3ade9d6
Adjekum Gideon <gidboa@gmail.com>
2013-10-09 16:39:35 +0000

    hey

--- Documents/java/Account.java
+++ Documents/java/Account.java
@@ -1,38 +1,62 @@
-public class Dogs{
+public class Account{
 
-    /* The list of attributes, 
-    defining the state of the object.*/
     private String accountHoldersName;
     private String accountNumber;
     private double amount;
-    private static final long BANK_ID = 80056322; 
+    private String history;
+    private static final long BANK_ID = 80056322;
+    private static int numberOfAccounts = 0;
+    protected double interestRate = 0;
 
-    /* This is a constructor. It desribes how to create a 
-    new Account object*/
-    public Account(String accountHoldersName, 
-        String accountNumber, 
-        double amount){
-    	this.accountHoldersName = accountHoldersName;
-    	this.accountNumber = accountNumber;
-     	this.amount = amount;
+    public Account(String accountHoldersName, String accountNumber, double amount){
+        this.accountHoldersName = accountHoldersName;
+        this.accountNumber = accountNumber;
+        this.amount = amount;
+        history ="";
+        numberOfAccounts++; 
     }
 
-    /* a class can have more than one constructor */
 
-    /* if we still want the standard constructor, we have to explicitly define it*/
- 
-    /* The list of methods forming the behavior of th
-    account class*/
+     public Account(String accountHoldersName, 
+        String accountNumber){
+        this.accountNumber = accountNumber;
+        this.accountHoldersName = accountHoldersName;
+        amount = 0.0;
+        history = "";
+        numberOfAccounts++;
+    }
+
     public void processTransfer(double amount){
-    	this.amount = this.amount + amount;
+        if(!isAmountPositive() && amount < 0){
+            updateHistory("Transaction could not be completed.");
+        }else{
+            this.amount = this.amount + amount;
+            updateHistory("Amount: "+amount+" was processed on your"
+                +" account. New balance is: "+this.amount);
+        }    	
+    }
+
+    private void updateHistory(String entry){
+        history = history + "\n" + entry;
     }
 
     public boolean isAmountPositive(){
     	return amount >= 0;
     }
 
+    public String toString(){
+        return "Bank Id: " + BANK_ID 
+        + "\nName: "+ accountHoldersName +
+        "\nAccount Number: "+ accountNumber + "\nAmount: " + amount;
+    }
+
+
+    public String getAccountHoldersName(){
+        return accountHoldersName;
+    }
+
     public static long getBankId(){
-    	return BANK_ID;
+        return BANK_ID;
     }
 
     public String getAccountNumber(){
@@ -43,15 +67,12 @@ public class Dogs{
         return amount;
     }
 
-    public String getName(){
-        return accountHoldersName;
+    public String getHistory(){
+        return history;
     }
 
-    /* This method defines how the textual representation of
-    your object will look like.*/
-    public String toString(){
-        return "Bank Id: " + BANK_ID 
-        + "\nName: "+ accountHoldersName +
-        "\nAccount Number: "+ accountNumber + "\nAmount: " + amount;
+    public static int getNumberOfAccounts(){
+         return numberOfAccounts;
     }
+ 
 }
